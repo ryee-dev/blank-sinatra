e26a7ac2a11d0c367df8633eb168b17b7e53de04
Misha Petrov <modesau@gmail.com>
2018-02-08 14:48:45 -0800

    minor changes

--- lib/project_creator.rb
+++ lib/project_creator.rb
@@ -2,8 +2,9 @@
 require 'fileutils'
 
 class Project
-  def initialize(project_name)
+  def initialize(project_name, classes)
     @project_name = project_name
+    @classes = classes
   end
 
   def create
@@ -18,13 +19,20 @@ class Project
     system "bundle install"
     FileUtils.touch("lib/#{@project_name}.rb")
     FileUtils.touch("spec/#{@project_name}_spec.rb")
-
+    file_contents = "#!/usr/bin/ruby\n\n"
+    @classes.each do |each_class|
+      each_class = each_class.capitalize
+      file_contents = file_contents.concat("class #{each_class}\n\nend\n\n")
+    end
+    File.open("lib/#{@project_name}.rb", 'w') { |file| file.write(file_contents) }
+    File.open("spec/#{@project_name}_spec.rb", 'w') { |file| file.write("require\('rspec')\nrequire('#{@project_name}'\)\nrequire('pry')\n\ndescribe('#{@project_name}'\) do\n  it(\"create new project\") do\n\n  end\nend") }
   end
 end
 
 puts "Enter a desired project name"
 project_name = gets.chomp
-puts "Enter a number of classes you want"
-num_of_classes = gets.chomp
-project = Project.new(project_name)
+puts "Enter classes for your .rb file separating them by a space"
+classes = gets.chomp
+classes = classes.split(" ")
+project = Project.new(project_name, classes)
 project.create
